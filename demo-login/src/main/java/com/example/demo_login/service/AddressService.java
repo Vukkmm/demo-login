89b1fb9e3547ab0901174b7756d67e7ec0dc896d
Nguyen Van Vu <vu27082002@gamil.com>
2024-09-09 17:32:33 +0700

    api saveAll address list

--- demo-login/src/main/java/com/example/demo_login/service/AddressService.java
+++ demo-login/src/main/java/com/example/demo_login/service/AddressService.java
@@ -3,10 +3,12 @@ package com.example.demo_login.service;
 import com.example.demo_login.dto.request.AddressRequest;
 import com.example.demo_login.entity.login.Address;
 
+import java.util.List;
+
 
 public interface AddressService {
     Address save(AddressRequest addressRequest);
 
-
+    List<Address> saveAll(List<AddressRequest> addressList);
 
 }
--- demo-login/src/main/java/com/example/demo_login/service/impl/AddressServiceImpl.java
+++ demo-login/src/main/java/com/example/demo_login/service/impl/AddressServiceImpl.java
@@ -4,22 +4,43 @@ import com.example.demo_login.dto.request.AddressRequest;
 import com.example.demo_login.entity.login.Address;
 import com.example.demo_login.repository.AddressRepository;
 import com.example.demo_login.service.AddressService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class AddressServiceImpl implements AddressService {
     private AddressRepository addressRepository;
+
+    @Transactional
     @Override
     public Address save(AddressRequest addressRequest) {
+        log.info("(save) addressRequest:{}", addressRequest);
         return addressRepository.save(
                 new Address(
                 addressRequest.getProvince(),
                 addressRequest.getDistrict(),
                 addressRequest.getWard()));
+    }
+
+    @Transactional
+    @Override
+    public List<Address> saveAll(List<AddressRequest> addressList) {
+        log.info("(saveAll) addressList:{}", addressList);
+        List<Address> addresses = new ArrayList<>();
+        for (AddressRequest i: addressList
+             ) {
+            addresses.add(new Address(i.getProvince(),
+                    i.getDistrict(),
+                    i.getWard()));
+        }
+        return addresses;
     }
 
 
